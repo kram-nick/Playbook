fac713e5f86f9ef6cb196b4f2470bdd16db49cab
kram-nick <110677569+kram-nick@users.noreply.github.com>
2023-06-08 14:26:08 +0300

    feat/preview

--- src/components/PreviewChapter/PreviewChapter.tsx
+++ src/components/PreviewChapter/PreviewChapter.tsx
@@ -1,34 +1,52 @@
-import Header from "../AppLayout/PrivateLayout/Header";  
-import { useAppSelector } from "../../core/hooks/useRedux";  
-import classNames from "classnames";  
+import Header from "../AppLayout/PrivateLayout/Header";
+import { useAppSelector } from "../../core/hooks/useRedux";
+import classNames from "classnames";
 
-const PreviewChapter = () => { 
+const PreviewChapter = () => {
   const { data } = useAppSelector((state) => state.app);
 
   return (
     <div className="w-full flex-1">
       <Header previewState={true} />
       <div className="p-[24px] gap-[32px] max-[1024px]:pt-[12px] max-[1024px]:px-[32px] max-[690px]:p-[0]">
- 
-        <div className="grid p-[24px] gap-y-[16px] rounded-[8px] bg-white shadow-free-trial border-[1px] border-solid border-header-bottom
+        <div
+          className="grid p-[24px] gap-y-[16px] rounded-[8px] bg-white shadow-free-trial border-[1px] border-solid border-header-bottom
            max-[690px]:py-[24px] max-[690px]:px-[16px] max-[690px]:border-[0px] max-[690px]:bg-transparent max-[690px]:shadow-none">
-           
-            <h1 className={classNames({ 
-                "text-[24px] font-poppins leading-normal font-semibold text-home-title" : true
-              })}>
-              {data.page_title ? (data.page_title) : '#1 Hire the best people through smart hiring'}
-            </h1>             
- 
-            {data && data.page_text ? (
-              <p className="text-[20px] text-simple-text leading-[32px] tracking-[-0.1px] max-w-[800px]">
-              {data.page_text ? data.page_text : ''}</p>
-            ) : ('')}      
-              <p className="text-[20px] text-simple-text leading-[32px] tracking-[-0.1px] max-w-[800px]">
-              Feel free to use any or all of these policies, and to modify them in whatever way makes sense for your company. We hope that by publishing them, other companies can benefit from our research and make their own policies more inclusive. All feedback and suggestions are appreciated. We’ve gotten several different perspectives to help arrive at these policies, but we are certainly missing many more and making mistakes as a result. This is a work in progress that we hope will get better with more time and more contributors. If these policies sound like a place where you want to work, please do look through our open career listings. The Obvious Employee Handbook is a derivative of the Tidepool Employee Handbook, which is itself derived from the Clef Employee Handbook. These are/were available to all via a Creative Commons license, 
-              and for that we are very thankful. We hope that many more people will suggest ways for us to improve these policies.</p>                  
-      
+          <h1
+            className={classNames({
+              "text-[24px] font-poppins leading-normal font-semibold text-home-title":
+                true,
+            })}>
+            {data.page_title
+              ? data.page_title
+              : "#1 Hire the best people through smart hiring"}
+          </h1>
+
+          {data && data.page_text ? (
+            <p className="text-[20px] text-simple-text leading-[32px] tracking-[-0.1px] max-w-[800px]">
+              {data.page_text ? data.page_text : ""}
+            </p>
+          ) : (
+            ""
+          )}
+          <p className="text-[20px] text-simple-text leading-[32px] tracking-[-0.1px] max-w-[800px]">
+            Feel free to use any or all of these policies, and to modify them in
+            whatever way makes sense for your company. We hope that by
+            publishing them, other companies can benefit from our research and
+            make their own policies more inclusive. All feedback and suggestions
+            are appreciated. We’ve gotten several different perspectives to help
+            arrive at these policies, but we are certainly missing many more and
+            making mistakes as a result. This is a work in progress that we hope
+            will get better with more time and more contributors. If these
+            policies sound like a place where you want to work, please do look
+            through our open career listings. The Obvious Employee Handbook is a
+            derivative of the Tidepool Employee Handbook, which is itself
+            derived from the Clef Employee Handbook. These are/were available to
+            all via a Creative Commons license, and for that we are very
+            thankful. We hope that many more people will suggest ways for us to
+            improve these policies.
+          </p>
         </div>
- 
       </div>
     </div>
   );
--- src/pages/PlaybookDetail/PlaybookDetail.tsx
+++ src/pages/PlaybookDetail/PlaybookDetail.tsx
@@ -1,120 +1,146 @@
 import { Link } from "react-router-dom";
-import AppHeader from "../../components/AppHeader";  
-import { Chapters, playbooks } from "../../core/constants/sidebar";
 import { useState } from "react";
-import BookBanner from "../../components/BookBanner";
 import classNames from "classnames";
 import { useTranslation } from "react-i18next";
 
-import arrow from "../../assets/photos/main/arrow-right.svg"; 
-import arrow_bread from "../../assets/photos/profile/right.svg";   
-import lock from "../../assets/photos/profile/lock.svg";   
-import back from "../../assets/photos/profile/back.svg";  
+import AppHeader from "../../components/AppHeader";
+import BookBanner from "../../components/BookBanner";
+
+import { useAppSelector } from "../../core/hooks/useRedux";
+import useHttpGet from "../../core/hooks/useHttpGet";
+import { APIRoutes } from "../../core/http";
+import { Data } from "../../core/models";
+
+import arrow from "../../assets/photos/main/arrow-right.svg";
+import arrow_bread from "../../assets/photos/profile/right.svg";
+import lock from "../../assets/photos/profile/lock.svg";
+import back from "../../assets/photos/profile/back.svg";
 import star from "../../assets/photos/profile/star.svg";
 
 const PlaybookDetail = () => {
-  const { t } = useTranslation();  
-  const [showDetail, handleView] = useState(false);   
-  const playbook = playbooks[0];
-  const chapters = Chapters;
-  
+  const { t } = useTranslation();
+  const [showDetail, handleView] = useState(false);
+  const [playbook, setPlaybook] = useState<Data.Playbook>();
+  const [pages, setPages] = useState<Data.Page[]>([]);
+
+  const { sharedId, reloadChecker } = useAppSelector((state) => state.helpers);
+
   const handleViewDetail = (open: boolean | any) => {
-    if(open){
+    if (open) {
       handleView(!showDetail);
     }
-  }
- 
+  };
+
+  useHttpGet<any>(`${APIRoutes.PLAYBOOKS}/${sharedId}`, {
+    dependencies: [],
+    resolve: (response) => {
+      setPlaybook(response?.data);
+    },
+  });
+
+  useHttpGet<any>(`${APIRoutes.PLAYBOOKS}/${sharedId}/pages`, {
+    resolve: (response: any) => {
+      setPages(response?.data);
+    },
+    dependencies: [sharedId, reloadChecker],
+  });
+
   return (
     <div className="bg-create-bg-main min-h-[100vh] w-[100%]">
-      <AppHeader profile={true} /> 
+      <AppHeader profile={true} />
 
       <div className="max-w-[1230px] px-[15px] mx-[auto] pb-[40px]">
         <ul className="breadcrumb flex items-center flex-wrap font-poppins pt-[24px] pb-[36px]">
           <li className="flex items-center gap-[4px]  mb-[4px]">
-            <Link to="/main" className="text-[14px] leading-[20px] tracking-[-0.1px] text-nav-txt-private">
+            <Link
+              to="/main"
+              className="text-[14px] leading-[20px] tracking-[-0.1px] text-nav-txt-private">
               Main Page
             </Link>
             <img src={arrow_bread} alt="" />
           </li>
           <li className="flex items-center text-[14px] leading-[20px] tracking-[-0.1px] mb-[4px] text-nav-txt-private">
-            <Link to="/profile" className="text-[14px] leading-[20px] tracking-[-0.1px] text-nav-txt-private">
+            <Link
+              to="/profile"
+              className="text-[14px] leading-[20px] tracking-[-0.1px] text-nav-txt-private">
               Chris’ Playbooks
             </Link>
-            <img src={arrow_bread} alt="" />            
+            <img src={arrow_bread} alt="" />
           </li>
           <li className="flex items-center text-[14px] leading-[20px] tracking-[-0.1px] mb-[4px] text-nav-txt-private">
-            {playbook.title}
-          </li>          
-        </ul> 
+            {playbook?.name}
+          </li>
+        </ul>
 
         <div className="max-w-[790px] mx-[auto]">
-          {!showDetail ? (
-            <div className="rounded-[8px] bg-white max-[1024px]:rounded-t-[0] shadow-free-trial 
+          {showDetail ? (
+            <div
+              className="rounded-[8px] bg-white max-[1024px]:rounded-t-[0] shadow-free-trial 
             border-[1px] border-solid border-header-bottom mb-[16px]">
-              <BookBanner preview={true} />
-
+              <BookBanner data={playbook} preview={true} />
               <div className="grid p-[24px] gap-y-[16px] mt-[-50px] max-[1024px]:px-[32px] max-[690px]:px-[16px]">
-                <h1 className="text-[32px] font-poppins font-bold text-home-title max-[690px]:text-[26px] max-[690px]:leading-[32px]" >
-                  {playbook.title}
-                </h1>             
-                {chapters?.length ? (
-                  <p className="text-[20px] text-simple-text leading-[32px] tracking-[-0.1px] max-w-[800px] 
-                    max-[690px]:text-[16px] max-[690px]:leading-[26px]">
-                  {chapters.length ? chapters[0].text : ''}</p>
-                ) : ('')}
-                {chapters?.length ? (
-                  <>
-                    {chapters.map((chapter: any, index: number) => (
-                      <button key={index} 
-                        onClick={() => handleViewDetail(chapter.open)}  
-                        className="flex items-center justify-between  rounded-[8px] bg-chapter-color px-[16px] py-[12px] 
+                <h1 className="text-[32px] font-poppins font-bold text-home-title max-[690px]:text-[26px] max-[690px]:leading-[32px]">
+                  {playbook?.name}
+                </h1>
+                {pages.map((chapter: Data.Page, index: number) => (
+                  <button
+                    key={index}
+                    onClick={() =>
+                      handleViewDetail(chapter.privacy === "private")
+                    }
+                    className="flex items-center justify-between  rounded-[8px] bg-chapter-color px-[16px] py-[12px] 
                           border-[1px] border-solid border-card-border gap-[30px] max-[690px]:p-[12px]">
-                        <span className="font-poppins text-[20px] text-home-title  font-medium leading-[28px] text-left 
+                    <span
+                      className="font-poppins text-[20px] text-home-title  font-medium leading-[28px] text-left 
                           max-[690px]:text-[16px] max-[690px]:leading-[24px]">
-                          {chapter.title}
-                        </span>
-                        <img src={chapter.open ? arrow : lock} alt="show" />
-                      </button>    
-                    ))}
-                  </>
-                ) : ('')}
-
-                {chapters?.length ? (
-                  <p className="text-[20px] text-simple-text leading-[32px] tracking-[-0.1px] max-w-[800px] 
-                    max-[690px]:text-[16px] max-[690px]:leading-[26px]">
-                  {chapters.length ? chapters[0].text : ''}</p>
-                ) : ('')}         
+                      {chapter.title}
+                    </span>
+                    <img
+                      src={chapter.privacy === "public" ? arrow : lock}
+                      alt="show"
+                    />
+                    {/* <div
+                      dangerouslySetInnerHTML={{ __html: chapter?.content }}
+                      className="text-[20px] text-simple-text leading-[32px] tracking-[-0.1px] max-w-[800px] 
+                    max-[690px]:text-[16px] max-[690px]:leading-[26px]"
+                    /> */}
+                  </button>
+                ))}
               </div>
             </div>
-          ) :( 
-            <div className="grid p-[24px] gap-y-[16px] rounded-[8px] bg-white shadow-free-trial border-[1px] 
+          ) : (
+            <div
+              className="grid p-[24px] gap-y-[16px] rounded-[8px] bg-white shadow-free-trial border-[1px] 
               border-solid border-header-bottom font-poppins max-[690px]:px-[16px]">
-                <button onClick={handleViewDetail}  
-                  className="flex items-center text-[16px] leading-[20px] font-medium text-buttons-bg gap-[4px]">
-                  <img src={back} alt="" /> {t<string>("BTNS.BACK")}
-                </button>
-                <h1 className={classNames({ 
-                    "text-[24px] leading-normal font-semibold text-home-title max-[690px]:text-[20px]" : true
-                  })}>
-                  {chapters[0].title}
-                </h1>             
-    
-                <p className="text-[20px] text-simple-text leading-[32px] tracking-[-0.1px] max-w-[800px] 
-                  max-[690px]:text-[16px] max-[690px]:leading-[26px]">
-                  {chapters[0].text}
-                </p>                  
-          
+              <button
+                onClick={handleViewDetail}
+                className="flex items-center text-[16px] leading-[20px] font-medium text-buttons-bg gap-[4px]">
+                <img src={back} alt="" /> {t<string>("BTNS.BACK")}
+              </button>
+              <h1
+                className={classNames({
+                  "text-[24px] leading-normal font-semibold text-home-title max-[690px]:text-[20px]":
+                    true,
+                })}>
+                {pages[0]?.title}
+              </h1>
+              <div
+                dangerouslySetInnerHTML={{ __html: pages[0]?.content }}
+                className="text-[20px] text-simple-text leading-[32px] tracking-[-0.1px] max-w-[800px] 
+                  max-[690px]:text-[16px] max-[690px]:leading-[26px]"
+              />
             </div>
           )}
           <div className="flex items-center gap-[8px] max-w-[330px] ml-[auto] mt-[16px]">
-            <button className="w-[46px] h-[46px] p-[12px] rounded-[6px] border-header-bottom border-[1px] border-solid bg-white"  >
+            <button className="w-[46px] h-[46px] p-[12px] rounded-[6px] border-header-bottom border-[1px] border-solid bg-white">
               <img src={star} alt="" className="w-[100%]" />
-            </button>  
-            <button  className="w-[calc(100%-56px)] h-[46px] px-[12px] rounded-[6px] border-btn-free border-[1px] 
+            </button>
+            <button
+              className="w-[calc(100%-56px)] h-[46px] px-[12px] rounded-[6px] border-btn-free border-[1px] 
                 border-solid shadow-free-trial bg-buttons-bg text-white text-[16px] font-medium">
               {t<string>("PROFILE.GET_FREE")}
             </button>
-          </div>           
+          </div>
         </div>
       </div>
     </div>
